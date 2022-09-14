d9ee6aa657edea1f81161b9899bb6da6904882aa
TAHAR Noureddine <71546633+EngrTAHAR-Noureddine@users.noreply.github.com>
2022-09-14 14:08:02 +0100

    add two projects

---
+++ client_one/lib/main.dart
@@ -0,0 +1,40 @@
+
+import 'package:client_one/MyProvider.dart';
+import 'package:dart_ipify/dart_ipify.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Login.dart';
+
+
+
+void main() async{
+  MyProvider().ipv4 = await Ipify.ipv4();
+
+  runApp(
+      MultiProvider(
+        providers: [
+          // this for root Changes
+          ChangeNotifierProvider(create: (_) => MyProvider())
+        ],
+        child: const MyApp(),
+      )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Login(),
+    );
+  }
+}
